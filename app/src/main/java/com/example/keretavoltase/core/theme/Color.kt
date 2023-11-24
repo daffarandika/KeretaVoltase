2cf2be8f11593ca7e58813984335985647baa72f
Daffa Randika <daffa.randika.3@gmail.com>
2023-11-24 15:15:27 +0700

    added some core components

--- app/src/main/java/com/example/keretavoltase/core/theme/Color.kt
+++ app/src/main/java/com/example/keretavoltase/core/theme/Color.kt
@@ -6,4 +6,5 @@ val DarkGreen = Color(0xFF011E11)
 val MediumGreen = Color(0xff014728)
 val LightGreen = Color(0xFFD1F9E7)
 
-val BrightYellow = Color(0xffE0D913)
+val White700 = Color(0xfffefefe)
+val Grey700 = Color(0xff3b3d4b)
